9fd9345d9b1e24419d740b4bed2d563c12c8f3e8
Manuel Mitasch <manuel@mitasch.com>
2023-11-04 17:57:38 +0100

    After Creation phase 2a

--- Scripts/wasm/ResetFX.ts
+++ Scripts/wasm/ResetFX.ts
@@ -1,7 +1,8 @@
 import * as dev from "./arduino";
 
 export function init(): void {
-
+    dev.setIMUEnabled(1);
+    dev.setBatterySendEnabled(0);
     dev.resetFX();
     dev.setFXIsoSpeed(1.0);
 }
